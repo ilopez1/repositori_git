b1326f0448d773f045dfd8e527453c8029bb2868
ivan <ivanlopezcarrillo4@gmail.com>
2016-10-06 16:58:48 +0200

    prova de conflicte repositori local

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ package net.ivanlopez;
 import java.util.Scanner;
 
 public class App {
-
+//Prova de conflicte repositori local
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner lector = new Scanner(System.in);
@@ -16,6 +16,7 @@ public class App {
     	   if(num < num_petit){
     		   num_p = num;
     	   }
+
             System.out.println("El numero mas pequeño es: " + num_p);
         }
 
